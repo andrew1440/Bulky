51cabba0e85db29fac105b51100c37d269ed5339
andrew1440 <drewodongo470@gmail.com>
2022-11-02 16:23:23 +0300

    added a delete button in index.cshtml

--- Bulky Book2/Controllers/CategoryController.cs
+++ Bulky Book2/Controllers/CategoryController.cs
@@ -84,7 +84,41 @@ namespace Bulky_Book2.wwwroot
             }
             return View(obj);
         }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
 
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+
+        }
     }
 
 
